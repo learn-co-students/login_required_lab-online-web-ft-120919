0ec3c78a2e911b6d7031678223a7912f1dff2dcb
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-03-11 15:20:26 -0700

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+  skip_before_action :require_login, only: [:show]
+
+  def show
+    if session.empty?
+      redirect_to '/new'
+    end
+
+  end
+
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
+end
